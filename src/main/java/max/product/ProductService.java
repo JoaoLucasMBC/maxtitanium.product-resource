77e8e0f5a1597304df0304900a92637927993801
EduardoMVAz <emendesvaz@gmail.com>
2024-05-29 11:18:13 -0300

    Circuit Breaker para o product resource

--- src/main/java/max/product/ProductService.java
+++ src/main/java/max/product/ProductService.java
@@ -11,17 +11,21 @@ import org.springframework.stereotype.Service;
 import max.product.exceptions.NotEnoughStockException;
 import max.product.exceptions.ProductNotFoundException;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class ProductService {
     
     @Autowired
     private ProductRepository productRepository;
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductCreate")
     @CachePut(value = "products", key = "#result.id()")
     public Product create(Product in) {
         return productRepository.save(new ProductModel(in)).to();
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductRead")
     @Cacheable(value = "products", key = "#id", unless = "#result == null")
     public Product read(String id) {
         Product product = productRepository.findById(id).map(ProductModel::to).orElse(null);
@@ -33,6 +37,7 @@ public class ProductService {
         return product;
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductUpdate")
     @CachePut(value = "products", key = "#result.id()", unless = "#result == null")
     public Product update(String id, Product in) {
         ProductModel dbProduct = productRepository.findById(id).orElse(null);
@@ -68,6 +73,7 @@ public class ProductService {
         return productRepository.save(new ProductModel(in)).to();
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductDelete")
     @CachePut(value = "products", key = "#id")
     public Product delete(String id) {
         final ProductModel dbProduct = productRepository.findById(id).orElse(null);
@@ -80,6 +86,7 @@ public class ProductService {
         return dbProduct.to();
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductList")
     public List<Product> list(Double min_price, Double max_price) {
 
         if (min_price != null && max_price != null) {
@@ -99,6 +106,7 @@ public class ProductService {
         
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductConsume")
     @CachePut(value = "products", key = "#result.id()", unless = "#result == null")
     public Product consume(String id, Integer quantity) {
         final ProductModel dbProduct = productRepository.findById(id).orElse(null);
@@ -115,6 +123,7 @@ public class ProductService {
         return productRepository.save(dbProduct).to();
     }
 
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackProductReplenish")
     @CachePut(value = "products", key = "#result.id()", unless = "#result == null")
     public Product replenish(String id, Integer quantity) {
         final ProductModel dbProduct = productRepository.findById(id).orElse(null);
@@ -125,5 +134,29 @@ public class ProductService {
 
         dbProduct.stock(dbProduct.stock() + quantity);
         return productRepository.save(dbProduct).to();
+    }
+
+    public void fallbackProductCreate(Product in, Throwable t) {
+        throw new RuntimeException("Failed to create product", t);
+    }
+
+    public void fallbackProductRead(String id, Throwable t) {
+        throw new RuntimeException("Failed to read product", t);
+    }
+
+    public void fallbackProductUpdate(String id, Product in, Throwable t) {
+        throw new RuntimeException("Failed to update product", t);
+    }
+    public void fallbackProductDelete(String id, Throwable t) {
+        throw new RuntimeException("Failed to delete product", t);
+    }
+    public void fallbackProductList(Double min_price, Double max_price, Throwable t) {
+        throw new RuntimeException("Failed to list products", t);
+    }
+    public void fallbackProductConsume(String id, Integer quantity, Throwable t) {
+        throw new RuntimeException("Failed to consume product", t);
+    }
+    public void fallbackProductReplenish(String id, Integer quantity, Throwable t) {
+        throw new RuntimeException("Failed to replenish product", t);
     }
 }
